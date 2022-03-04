4f46ed3a04b08cc5afd7f0727d535bf6f8465efa
Mar <martadeharo.94@gmail.com>
2022-03-04 11:35:37 +0100

    nuevosEjercicios

--- JS/ejercicios/ejercicio33.js
+++ JS/ejercicios/ejercicio33.js
@@ -2,3 +2,14 @@
 Crear un array de tamaño 10 y que guardará números enteros introducidos por teclado.
 Tras introducirlos todos, imprimirá cada índice junto con el valor al que corresponda.*/
 
+console.clear();
+const prompt = require("prompt-sync")();
+
+let miArray = [];
+
+const number = 11;
+
+for (let i = 1; i < number; i++) {
+    miArray[i] = Number(prompt("Número de entrada: "));
+    console.log("El valor ", i ,"es ", miArray[i]);
+}
