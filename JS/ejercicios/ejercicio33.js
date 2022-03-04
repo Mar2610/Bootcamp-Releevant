dc7686d3d22b21a11e0970d7e5e74c84c563c771
Mar <martadeharo.94@gmail.com>
2022-03-04 11:18:47 +0100

    ejercicio33

---
+++ JS/ejercicios/ejercicio33.js
@@ -0,0 +1,4 @@
+/* Ejercicio 33
+Crear un array de tamaño 10 y que guardará números enteros introducidos por teclado.
+Tras introducirlos todos, imprimirá cada índice junto con el valor al que corresponda.*/
+
