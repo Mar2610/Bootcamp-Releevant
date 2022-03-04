4da65346027a3c294d56731c7039c6487c947c2d
Mar <martadeharo.94@gmail.com>
2022-03-04 12:41:24 +0100

    Ejercicios

--- JS/ejercicios/ejercicio35.js
+++ JS/ejercicios/ejercicio35.js
@@ -3,3 +3,22 @@ Crear un array de números donde le indicaremos el tamaño por teclado. Rellenar
 con números aleatorios entre 0 y 9. Posteriormente, mostrará por pantalla el valor de cada posición
 junto con su índice y finalmente, la suma de todos los valores.*/
 
+console.clear();
+
+const prompt = require("prompt-sync")();
+
+let miArray = [];
+
+const longitud = Number(prompt("Longitud: ")); 
+let sum = 0;
+
+for (i=0; i<longitud; i++) {
+    miArray[i] = Math.floor(Math.random() * 10) + 1;
+    sum += miArray[i];
+    console.log("La posición es ", i ,"y el valor es ", miArray[i]);
+}
+
+console.log("La suma es ", sum);
+
+
+
