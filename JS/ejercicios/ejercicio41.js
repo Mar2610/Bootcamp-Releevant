da348968013b283d63fe997a5e6780e65bf1f115
Mar <martadeharo.94@gmail.com>
2022-03-06 19:57:44 +0100

    Ejercicios

---
+++ JS/ejercicios/ejercicio41.js
@@ -0,0 +1,37 @@
+/* Ejercicio 41
+Dado un array de N números enteros que se generen aleatoriamente, hacer un algoritmo que:
+a) Obtenga cuántos números son mayores que 0.
+b) Calcule el promedio de los números positivos.
+c) Obtenga el promedio de todos los números.*/
+
+console.clear();
+const prompt = require("prompt-sync")();
+
+const N = Number(prompt("Tamaño del array: "));
+let miArray = [];
+let positive = 0;
+let totalSum = 0;
+let positiveSum = 0;
+
+for (let i = 0; i < N; i++) {
+  miArray[i] = randomValue(-100, 100);
+  if (miArray[i] > 0) {
+    positive++;
+    positiveSum += miArray[i];
+  }
+  totalSum += miArray[i];
+}
+
+function randomValue(min, max) {
+  if (min < 0) {
+    return min + Math.floor(Math.random() * (Math.abs(min) + max));
+  } else {
+    return min + Math.floor(Math.random() * max);
+  }
+}
+
+console.log(positive);
+console.log("Array:", miArray);
+console.log("Números positivos:", positive);
+console.log("Promedio números positivos:", positiveSum / positive);
+console.log("Promedio de todos los números:", totalSum / miArray.length);
---
+++ JS/ejercicios/ejercicio43.js
@@ -0,0 +1,5 @@
+/* Ejercicio 43
+Partir del ejercicio 28 pero esta vez realizar un reloj digital completo que nunca pare. Tendrá la
+estructura horas:minutos:segundos. Ejemplo de salida: 23:15:39
+Nota: deberás utilizar “Esperar” y “Limpiar pantalla”.*/
+
