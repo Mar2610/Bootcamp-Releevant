6e5373ed48903e1e54c163caeadc172e7bf9a41e
Mar <martadeharo.94@gmail.com>
2022-03-16 20:53:43 +0100

    Ejercicios

--- JS/ejercicios/ejObjetos/prueba.js
+++
@@ -1,6 +0,0 @@
-
-const miArray = [{animal: 1, patas:4}, {animal: 2, patas:4}, {animal: 3, patas:4}, {animal: 4, patas:4}];
-
-const reducir = miArray.reduce((total,item) => item.animal * item.patas,0);
-
-console.log(reducir);
