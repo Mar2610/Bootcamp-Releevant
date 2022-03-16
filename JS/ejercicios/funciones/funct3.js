06c93fcfa3621d51161fd84123c46cb9ece553de
Mar <martadeharo.94@gmail.com>
2022-03-16 13:26:04 +0100

    Ejercicios

--- JS/ejercicios/funciones/funct3.js
+++ JS/ejercicios/funciones/funct3.js
@@ -1,13 +1,13 @@
 
 /*Crear una función que reciba un número entero y muestre un error si el tipo de dato pasado es de otro tipo.*/
 
-// function type (number) {
-//     if(isNaN(number)) { // isNaN es una función
-//         return "Error";
-//     }
-// }
+function type (number) {
+    if(isNaN(number)) { // isNaN es una función
+        return "Error";
+    }
+}
 
-// console.log(type("Lechuga"));
+console.log(type("Lechuga"));
 
 
 // Función flecha
--- JS/ejercicios/funciones/funct4.js
+++ JS/ejercicios/funciones/funct4.js
@@ -3,15 +3,15 @@
 como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla
 el resultado devuelto por la función.*/
 
-// function parOimpar (number) {
-//     if (number % 2 === 0) {
-//         return "par";
-//     } else {
-//         return "impar";
-//     }
-// }
+function parOimpar (number) {
+    if (number % 2 === 0) {
+        return "par";
+    } else {
+        return "impar";
+    }
+}
 
-// console.log(parOimpar(7));
+console.log(parOimpar(7));
 
 // Función flecha
 const parOimpar = (number) => (number % 2 === 0 ) ? "par" : "impar";
