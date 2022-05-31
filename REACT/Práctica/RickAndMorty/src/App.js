b03c223ad469df8b943d596a383241e4a4a085d2
Mar <martadeharo.94@gmail.com>
2022-05-31 16:12:33 +0200

    React

--- REACT/Práctica/RickAndMorty/src/App.js
+++ REACT/Práctica/RickAndMorty/src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import './Components/Characters'
+import "./App.css";
 import { useState, useEffect } from "react";
+import Characters from "./Components/Characters";
+import Pagination from "./Components/Pagination";
 
 function App() {
-
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(function () {
     async function fetchApi() {
-      const response = await fetch(
-        "https://rickandmortyapi.com/api/character"
-      );
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
       const json = await response.json();
-      setCharacters(json);
+      setCharacters(json.results);
     }
     fetchApi();
-  }, []);
+  }, [page]);
 
   return (
-    <div className="App">
-     
+    <div>
+      <Characters character={characters} />
+      <Pagination />
     </div>
   );
 }
--- REACT/Práctica/RickAndMorty/src/Components/Characters/Characters.js
+++ REACT/Práctica/RickAndMorty/src/Components/Characters/Characters.js
@@ -1,14 +1,22 @@
 export default function Characters({ character }) {
+  console.log(character);
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title"></h5>
-        <p className="card-text"></p>
-        <a  className="btn btn-primary">
-          
-        </a>
+    <>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-5">
+          {character.map((char) => (
+            <div className="card justify-content-between">
+              <img className="card-img-top" src={char.image} />
+              <div className="card-body">
+                <h5 className="card-title">{char.name}</h5>
+                <p className="card-text">{char.gender}</p>
+                <a className="btn btn-primary m-1">Detalles</a>
+                <a className="btn btn-danger row m-1">Eliminar</a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
